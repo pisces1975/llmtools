17216907ac69d1209e5cfb03011af9d030210d92
JeffreyZ <zhouzhen@nstc.com.cn>
2024-03-23 00:21:34 +0800

    move session key management to a MySQL DB

--- llmtool_service.py
+++ llmtool_service.py
@@ -4,14 +4,32 @@ from utilities import logger
 from flask_cors import CORS
 import sys
 from xinference.client import Client
-from datetime import datetime, timedelta
-import schedule
+import mysql.connector
 
 #app = Flask(__name__)
 mylogger = logger.Logger(name='llmtool', debug=True).logger  
 app = Flask(__name__)  
 CORS(app)
 
+mysql_db_config = {
+    "host": "10.101.9.50",
+    "user": 'root',
+    "password": 'newpass',
+    "database": 'codebase'
+}
+
+db_connection = mysql.connector.connect(**mysql_db_config)
+db_cursor = db_connection.cursor()
+if db_connection.is_connected():
+    mylogger.debug("Successfully connect mysql database")
+
+mylogger.debug(mysql_db_config)
+def execute_query(query, params=()):
+    db_cursor.execute(query, params)
+    if query.strip().startswith("SELECT"):
+        pass
+    else:
+        db_connection.commit() 
 # 使用字典模拟用户存储，实际项目中应使用数据库
 users_db = {
     "admin": {"password": "123456"},
@@ -42,15 +60,17 @@ def create_embedding_bge(sentence):
 def login():
     username = request.json.get('username')
     password = request.json.get('password')
+    clear_expired_user_keys()  # 清理过期的userKey
 
     if username in users_db and users_db[username]['password'] == password:
         # 生成一个随机的session key
         session_key = str(uuid.uuid4())
         
-        # 将username和session_key关联并保存
-        sessions[session_key] = {'username': username}
-        sessions[session_key]['last_refresh'] = datetime.now()
-        
+        query = "DELETE FROM user_sessions WHERE username = %s"
+        execute_query(query, (username,))
+        query = "INSERT INTO user_sessions (userKey, username) VALUES (%s, %s)"
+        execute_query(query, (session_key, username))
+                
         # 返回成功响应，包含session key
         response = jsonify({"status": "success", "key": session_key})
         mylogger.debug(f'User {username} has entered, key is [{session_key}]')
@@ -66,40 +86,37 @@ def test():
     password = request.json.get('password')
     userKey = request.json.get('userKey')
 
-    if userKey is not None and sessions[userKey] is not None:
-        mylogger.debug(f'Find correct key for user {username}')
-        response = jsonify({"status": "success", "content": f'Your username is {sessions[userKey]}'})
-        return response, 200
+    mylogger.debug(f"test ==> {userKey}")
+    query = 'SELECT username, last_refresh FROM user_sessions WHERE userKey = %s'
+    execute_query(query, (userKey,))
+    res = db_cursor.fetchone()
+    if res:
+        return jsonify({"status": "success", "content": f'Your username is {res[0]}'}), 200
     else:
-        mylogger.error(f'Key error')
         return jsonify({"status": "fail", "message": "请先登录"}), 401
-
+        
 # 添加checkLogin服务
 @app.route('/checkLogin', methods=['POST'])
 def check_login():
     user_key = request.json.get('userKey')
-    
-    if user_key in sessions:
-        # 更新userKey的刷新时间
-        sessions[user_key]['last_refresh'] = datetime.now()
-        
-        # 返回username
-        username = sessions[user_key]['username']
-        return jsonify({"status": "success", "username": username}), 200
+    mylogger.debug(f"checkLogin ==> {user_key}")
+    clear_expired_user_keys()
+    query = 'SELECT username, last_refresh FROM user_sessions WHERE userKey = %s'
+    execute_query(query, (user_key,))
+    res = db_cursor.fetchone()
+    if res:
+        return jsonify({"status": "success", "username": res[0]}), 200
     else:
-        return jsonify({"status": "fail", "message": "验证失败"}), 401
+        return jsonify({"status": "fail", "message": "验证失败"}), 401           
     
 def generate_float_list():
     float_list = [round(i * 0.01, 2) for i in range(1, 1025)]
     return float_list
 
-# 定时清除过期userKey
-def clear_expired_sessions():
-    current_time = datetime.now()
-    for key, value in sessions.items():
-        last_refresh_time = value.get('last_refresh')
-        if last_refresh_time and current_time - last_refresh_time > timedelta(hours=1):
-            del sessions[key]
+def clear_expired_user_keys():    
+    # 删除1小时前更新的userKey
+    query = "DELETE FROM user_sessions WHERE last_refresh < (NOW() - INTERVAL 1 HOUR)"
+    execute_query(query)
 
 @app.route('/genvec', methods=['POST'])
 def get_float_list():
@@ -111,7 +128,6 @@ def get_float_list():
 
 if __name__ == '__main__':
     host = 'localhost'
-    schedule.every().hour.do(clear_expired_sessions)
     app.run(debug=False, host=host, port=5111)
 
 # 在实际应用中，还需实现logout等接口，清除对应session_key的数据，并且考虑session过期等问题
